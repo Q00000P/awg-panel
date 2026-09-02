a8268bb2bbf3283d86112240f90b5241ea508b60
Q00000P <github@p5q.ru>
2026-09-02 22:02:58 +0300

    Fix badge SFC parse

--- src/app/components/ClientCard/Interface.vue
+++ src/app/components/ClientCard/Interface.vue
@@ -3,7 +3,7 @@
     v-if="showBadge"
     class="inline-block rounded bg-gray-100 px-1.5 py-0.5 text-xs text-gray-600 dark:bg-neutral-700 dark:text-neutral-300"
   >
-    {{ client.interfaceId }}<template v-if="version"> · {{ version }}</template>
+    {{ badgeText }}
   </span>
 </template>
 
@@ -19,9 +19,10 @@ const showBadge = computed(
   () => (interfaces.value?.length ?? 0) > 1 && !!props.client.interfaceId
 );
 // /api/interfaces уже отдаёт isAwg31 (по наличию headerProtectionKey)
-const version = computed(() => {
-  const i = interfaces.value?.find((x) => x.name === props.client.interfaceId);
-  if (!i) return '';
-  return i.isAwg31 ? 'AWG 3.1' : 'AWG 2.0';
+const badgeText = computed(() => {
+  const id = props.client.interfaceId;
+  const i = interfaces.value?.find((x) => x.name === id);
+  if (!i) return id;
+  return `${id} \u00b7 ${i.isAwg31 ? 'AWG 3.1' : 'AWG 2.0'}`;
 });
 </script>
