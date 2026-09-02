cd1efaf0f5f5da5cd66b190f754136d8530818fa
Q00000P <github@p5q.ru>
2026-09-02 21:39:06 +0300

    AWG version in interface badge

--- src/app/components/ClientCard/Interface.vue
+++ src/app/components/ClientCard/Interface.vue
@@ -3,7 +3,7 @@
     v-if="showBadge"
     class="inline-block rounded bg-gray-100 px-1.5 py-0.5 text-xs text-gray-600 dark:bg-neutral-700 dark:text-neutral-300"
   >
-    {{ client.interfaceId }}
+    {{ client.interfaceId }}<template v-if="version"> · {{ version }}</template>
   </span>
 </template>
 
@@ -18,4 +18,10 @@ const { data: interfaces } = await useFetch('/api/interfaces');
 const showBadge = computed(
   () => (interfaces.value?.length ?? 0) > 1 && !!props.client.interfaceId
 );
+// /api/interfaces уже отдаёт isAwg31 (по наличию headerProtectionKey)
+const version = computed(() => {
+  const i = interfaces.value?.find((x) => x.name === props.client.interfaceId);
+  if (!i) return '';
+  return i.isAwg31 ? 'AWG 3.1' : 'AWG 2.0';
+});
 </script>
